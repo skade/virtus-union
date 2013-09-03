454fa18272ceba5a2e85753df2795d8d5ee513f2
Florian Gilcher <florian.gilcher@asquera.de>
2013-09-03 22:23:31 +0200

    Documentation

--- lib/virtus/attribute/union.rb
+++ lib/virtus/attribute/union.rb
@@ -3,6 +3,9 @@ module Virtus
     class Union < EmbeddedValue
       primitive Virtus::Union
 
+      # Init an instance of Virtus::Attribute::Union
+      #
+      # @api private
       def initialize(*)
         super
         @union_attributes = AttributeSet.new
@@ -12,12 +15,29 @@ module Virtus
         @union_attributes << Attribute.build(:discriminator, Symbol)
       end
 
+      # Reads types and discriminator values from the configuration
+      # type.
+      #
+      # @param [Virtus::Union]
+      #
+      # @param [Hash]
+      #
+      # @return [Hash]
+      #
+      # @api private
       def self.merge_options(type, options)
+        super
         options[:types] = type.types
         options[:discriminator] = type.discriminator
         options
       end
 
+      # Coerce a given value to the correct type given in the
+      # Union configuration. Return `nil` if unknown.
+      #
+      # @param [Object] value
+      #
+      # @return [Virtus] The coerced value.
       def coerce(value)
         type_tag = value[options[:discriminator]]
         type_tag = @union_attributes[:discriminator].coerce(type_tag)
--- lib/virtus/union.rb
+++ lib/virtus/union.rb
@@ -3,20 +3,48 @@ require "virtus/union/version"
 require "virtus/attribute/union"
 
 module Virtus
+  # Proxy type to define a Union virtus attribute.
+  #
+  # @example
+  #   class MyClass
+  #     include Virtus
+  #     attribute :union, Union[:type, AVirtusType, AnotherVirtusType]
+  #   end
   class Union
     include Enumerable
 
-    attr_accessor :types, :discriminator
+    # The types used in this union.
+    attr_accessor :types
+    # The discriminator attribute name.
+    attr_accessor :discriminator
 
+    # Constructs a new `Virtus::Union` object, holding a
+    # discriminator value and the types used in this union.
+    #
+    # @param [Symbol] discrimitator The key used to detect the
+    # object type.
+    # @param [Hash<Symbol,Virtus>] types The types allowed in this
+    # union, keyed by the discriminator value.
     def initialize(discriminator, types)
       self.discriminator = discriminator
       self.types = types
     end
 
+    # Sugar method to construct a `Virtus::Union` object.
+    #
+    # @see #initialize
+    #
+    # @param [Symbol] discrimitator The key used to detect the object type.
+    # @param [Hash<Symbol,Virtus>] types The types allowed in this union, keyed by the discriminator value.
+    #
+    # @return [Virtus::Union]
     def self.[](discrimitator, types)
       new(discrimitator, types)
     end
 
+    # Iterates over all members of `types`.
+    #
+    # @return [Hash<Symbol,Virtus>]
     def each(&block)
       types.each(&block)
     end
